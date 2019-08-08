aa82317c2988f722566e3af6d07e5edf3e0a3f4b
kminkyung <mminkyung.k@gmail.com>
2019-08-08 17:45:25 +0900

    navi 작업중

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// sidebar scroll event
+$(window).scroll(function(){
+	var scTop = $(this).scrollTop();
+	if(scTop > 50) $(".side-black").stop().animate({"left": 0}, 1000);
+	else $(".side-black").stop().animate({"left": "-100%"}, 1000);
+	console.log(scTop);
+});
+
+//side-bar hover event
+$(".side-bar").mouseenter(function(){
+	$(this).stop().animate({"opacity":0, "height":"35%"}, 300);
+	$(".side-clay").stop().animate({"opacity":1, "height":"35%"}, 300);
+	$(".side-tit").stop().animate({"bottom":"210px"}, 300);
+});
+$(".side-bar").mouseleave(function(){
+	$(this).stop().animate({"opacity":1, "height":"27%"}, 300);
+	$(".side-clay").stop().animate({"opacity":0, "height":"27%"}, 300);
+	$(".side-tit").stop().animate({"bottom":"250px"}, 300);
+});
+
+
+// button hover event
+$(".btn-explore").mouseenter(function(){
+	$(this).find(".btn-hover").stop().animate({"height":"100%"}, 100);
+});
+$(".btn-sample").mouseenter(function(){
+	$(this).css("color","white");
+	$(this).find(".btn-hover").stop().animate({"height":"100%"}, 100);
+});
+$(".btn-explore").mouseleave(function(){
+	$(this).find(".btn-hover").stop().animate({"height":0}, 100);
+});
+$(".btn-sample").mouseleave(function(){
+	$(this).css("color","black");
+	$(this).find(".btn-hover").stop().animate({"height":0}, 100);
+});
+
